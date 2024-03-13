931277f2026cfd2e20ea7bec9b14dd6e2f4ee248
Ishan1504 <ishan.malawade15@gmail.com>
2024-03-13 12:11:13 +0530

    Feature updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,14 +207,4 @@
     aos_init();
   });
 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbyKFidLdXSudl-DGeci2gW5_MH2PxjWHnyalXeGw94q9KHGJZ4ya7G41KaYS6zAb-4twA/exec'
-  const form = document.forms['google-sheet']
-  
-  form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(_response => $("#form_alerts").html("<div class='alert alert-success'>Contact message sent successfully.</div>"))
-      .catch(_error => $("#form_alerts").html("<div class='alert alert-danger'>Contact message not sent.</div>"))
-  })
-
 })(jQuery);
